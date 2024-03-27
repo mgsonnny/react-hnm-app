06cf929a3efb041179bb5b34be4062eabe9334fa
mgsonny <smg80zzang@daum.net>
2024-03-27 22:12:34 +0900

    first-commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://lp2.hm.com/hmgoepprod?set=source[/c0/c6/c0c6e7e10a3dddd7922f2f6b6f15bda813efdb65.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[w],hmver[4]&call=url[file:/product/main]'/>
+      <div>Conscious choic</div>
+      <div>밸티드 트윌 코트</div>
+      <div>\99000</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
