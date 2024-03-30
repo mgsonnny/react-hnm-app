cf7e1e5b405b963f064008ca530cf4fce9af642b
mgsonny <smg80zzang@daum.net>
2024-03-30 23:25:37 +0900

    seven-commt

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -8,11 +8,11 @@ const ProductCard = ({item}) => {
     }
   return (
     <div className='card' onClick={showDetail}>
-      <img width={300}  src={item?.img}/>
-      <div>{item?.choice == true?"Conscious choice":""}</div>
+      <img className='card-img' width={300}  src={item?.img}/>
+      <div className='choice'>{item?.choice == true?"Conscious choice":""}</div>
       <div>{item?.title}</div>
       <div>{item?.price}원</div>
-      <div>{item?.new == true?"신제품":""}</div>
+      <div className='new-product'>{item?.new == true?"신제품":""}</div>
     </div>
   )
 }
